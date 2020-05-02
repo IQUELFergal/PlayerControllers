1cf6c8a27f13ef13011a2031bad345223f4bea13
IQUELFergal <fergal.iquel@gmail.com>
2020-05-02 19:33:26 +0200

    Update TopDownPlayerController.cs

--- TopDownPlayerController.cs
+++ TopDownPlayerController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Animator))]
 public class TopDownPlayerController : MonoBehaviour
 {
     [Header("Rotation")]
